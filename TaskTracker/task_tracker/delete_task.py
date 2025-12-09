a2fd0f2e2163b4543133bfef616c15227d6f0fb1
animbadger <anim.ansah.stephen@gmail.com>
2025-12-09 17:13:18 +0000

    modified delete task function

---
+++ TaskTracker/task_tracker/delete_task.py
@@ -0,0 +1,25 @@
+from task_tracker.utils import load_tasks, TASK_FILE, save_task
+import os
+
+def delete_task(task_id: int) -> bool :
+    """
+    Delete a task by ID.
+
+    :param task_id: ID of the task to delete
+    :return: True if deleted, False if not found
+    """
+    if not os.path.exists(TASK_FILE):
+        print('No tasks found')
+        return False
+    
+    tasks = load_tasks()
+
+    for task in tasks:
+        if task['id'] == task_id:
+            tasks.remove(task)
+            save_task(tasks)
+            print('Task successfully removed')
+            return True
+        
+    print(f'Task with id {task_id} not found')
+    return False
