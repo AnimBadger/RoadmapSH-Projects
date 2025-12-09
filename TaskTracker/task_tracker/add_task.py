c7265bde1ffd0d9f2b3c7dffccd3f842a873435e
animbadger <anim.ansah.stephen@gmail.com>
2025-12-09 17:13:01 +0000

    modified add task function

--- TaskTracker/task_tracker/add_task.py
+++ TaskTracker/task_tracker/add_task.py
@@ -1,8 +1,8 @@
 from task_tracker.task import Task
 from task_tracker.utils import load_tasks, save_task
 import random
+from task_tracker.utils import TASK_ID_LIMIT
 
-TASK_FILE = 'tasks.json'
 
 def add_task(description: str) -> Task :
     tasks = load_tasks()
@@ -21,7 +21,7 @@ def generate_task_id(tasks: list[dict]) -> int:
     existing_ids = {task["id"] for task in tasks}
 
     while True:
-        task_id = random.randint(1, 9999)
+        task_id = random.randint(1, TASK_ID_LIMIT)
         if task_id not in existing_ids:
             return task_id
         
