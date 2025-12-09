ab6cca0208aaafa2e06518e86aecc991bb4d2cef
animbadger <anim.ansah.stephen@gmail.com>
2025-12-09 18:48:05 +0000

    added list tasks functionality

---
+++ TaskTracker/task_tracker/list_tasks.py
@@ -0,0 +1,18 @@
+from task_tracker.utils import load_tasks, TASK_FILE
+import os
+
+def list_tasks() -> list:
+    if not os.path.exists(TASK_FILE):
+        print('No tasks found')
+        return []
+
+    tasks = load_tasks()
+    print("\nYour Tasks:\n" + "-"*50)
+    for task in tasks:
+        print(f"ID       : {task['id']}")
+        print(f"Description: {task['description']}")
+        print(f"Status     : {task['status']}")
+        print(f"Created At : {task['createdAt']}")
+        print(f"Updated At : {task['updatedAt']}")
+        print("-"*50)
+    return tasks
